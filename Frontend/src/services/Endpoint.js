a3119120cea8a19daaeb3f579932276ce7d78ea8
vasisht5001 <162408664+vasisht5001@users.noreply.github.com>
2025-01-02 16:01:36 +0530

    Update Endpoint.js

--- Frontend/src/services/Endpoint.js
+++ Frontend/src/services/Endpoint.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-export const BaseUrl='http://localhost:5000'
+export const BaseUrl='https://explore-kerala-backend-gy2i.onrender.com'
 const instance = axios.create({
     baseURL:BaseUrl,
     // headers: {
